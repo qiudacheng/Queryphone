58c4ea5815f4426a5a20f134754355dff1eba5fb
admin <gdptc123@163.com>
2019-06-18 09:10:09 +0800

    commit

---
+++ app/src/main/java/cn/edu/gdpt/queryphone/Activity/FirstActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.gdpt.queryphone.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.gdpt.queryphone.R;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+    }
+}
---
+++ app/src/main/java/cn/edu/gdpt/queryphone/Activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdpt.queryphone.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.gdpt.queryphone.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
