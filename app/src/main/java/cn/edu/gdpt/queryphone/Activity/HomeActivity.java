eb01083ebd6d7d88e759f3cc6958eb40483f1c25
admin <1096208568@qq.com>
2019-06-20 16:17:38 +0800

    Login Register popuWindow

--- app/src/main/java/cn/edu/gdpt/queryphone/Activity/HomeActivity.java
+++ app/src/main/java/cn/edu/gdpt/queryphone/Activity/HomeActivity.java
@@ -6,9 +6,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +25,8 @@ public class HomeActivity extends AppCompatActivity {
     private DrawerLayout drawerlayout;
     ImageView menu;
     private TextView tv_userName;
+    private ImageView img_query;
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +63,72 @@ public class HomeActivity extends AppCompatActivity {
         String user = intent.getStringExtra("user");
         tv_userName = headView.findViewById(R.id.tv_user);
         tv_userName.setText(user);
+        img_query=(ImageView)findViewById(R.id.img_query);
+        img_query.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeActivity.this,TelephoneEnquiryActivity.class);
+                startActivity(intent);
+            }
+        });
+        final LinearLayout chat,frinend,see,pay,help;
+        ImageButton btn_popu=(ImageButton)findViewById(R.id.btn_popu);
+        btn_popu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View myview= LayoutInflater.from(HomeActivity.this).inflate(R.layout.popowindow,null);
+                final PopupWindow mypopwindow=new PopupWindow(myview, ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT,true);
+                mypopwindow.setTouchable(true);
+                mypopwindow.setOutsideTouchable(true);
+                mypopwindow.showAsDropDown(view);
+                LinearLayout see=(LinearLayout)myview.findViewById(R.id.ll_see);
+                LinearLayout pay=(LinearLayout)myview.findViewById(R.id.ll_pay);
+                LinearLayout help=(LinearLayout)myview.findViewById(R.id.ll_help);
+                LinearLayout chat=(LinearLayout)myview.findViewById(R.id.ll_chat);
+                LinearLayout frinend=(LinearLayout)myview.findViewById(R.id.ll_friend);
+                chat.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(HomeActivity.this,"添加朋友",Toast.LENGTH_LONG).show();
+                        mypopwindow.dismiss();
+
+                    }
+                });
+                frinend.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(HomeActivity.this,"听歌识曲",Toast.LENGTH_LONG).show();
+                        mypopwindow.dismiss();
+
+                    }
+                });
+                see.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(HomeActivity.this,"分享给朋友",Toast.LENGTH_LONG).show();
+                        mypopwindow.dismiss();
+
+                    }
+                });
+                pay.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(HomeActivity.this,"设置",Toast.LENGTH_LONG).show();
+                        mypopwindow.dismiss();
+
+                    }
+                });
+                help.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(HomeActivity.this,"帮助与反馈",Toast.LENGTH_LONG).show();
+                        mypopwindow.dismiss();
+
+                    }
+                });
+            }
+        });
+
 
     }
 }
