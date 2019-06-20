28f7ea2972aa4d934904651d822e51e7f6840783
admin <1096208568@qq.com>
2019-06-20 16:41:46 +0800

    QQinquiry

---
+++ app/src/main/java/cn/edu/gdpt/queryphone/Activity/QQInquiry.java
@@ -0,0 +1,15 @@
+package cn.edu.gdpt.queryphone.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.edu.gdpt.queryphone.R;
+
+public class QQInquiry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qqinquiry);
+    }
+}
